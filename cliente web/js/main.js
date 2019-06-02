a6b4918826112634748cff46982c50ff3a9adedb
Rodrigo <rodrigoaguilar96@gmail.com>
2019-06-02 19:57:59 -0300

    modificaciones js

--- cliente web/js/main.js
+++ cliente web/js/main.js
@@ -13,24 +13,31 @@ function mostrarpag(url) {
             container.innerHTML = t;
   
             if (url==="div/Cliente.html"){
-              document.querySelector('.registrarse').addEventListener("click", function(){mostrarpag("server/registrar.html")});
+              document.querySelector(".create").addEventListener("click", create);
+              document.querySelector(".delete").addEventListener("click", delate);
+              document.querySelector(".edit").addEventListener("click", edit);
             }
             if (url==="div/Alquileres.html"){
-              document.querySelector(".upload").addEventListener("click", create);
+              document.querySelector(".create").addEventListener("click", create);
               document.querySelector(".delete").addEventListener("click", delate);
               document.querySelector(".edit").addEventListener("click", edit);
             }
             if (url==="div/Posiciones.html"){
-                document.querySelector('.registrarse').addEventListener("click", function(){mostrarpag("server/registrar.html")});
+                document.querySelector(".create").addEventListener("click", create);
+                document.querySelector(".delete").addEventListener("click", delate);
+                document.querySelector(".edit").addEventListener("click", edit);
             }
             if (url==="div/Filas-Estanterias.html"){
-                document.querySelector('.registrarse').addEventListener("click", function(){mostrarpag("server/registrar.html")});
+                document.querySelector(".create_fila").addEventListener("click", create);
+                document.querySelector(".delete_fila").addEventListener("click", delate);
+                document.querySelector(".edit_fila").addEventListener("click", edit);
+                document.querySelector(".create_estanteria").addEventListener("click", create);
+                document.querySelector(".delete_estanteria").addEventListener("click", delate);
+                document.querySelector(".edit_estanteria").addEventListener("click", edit);
             }
             if (url==="div/Pallets.html"){
-                document.querySelector('.registrarse').addEventListener("click", function(){mostrarpag("server/registrar.html")});
             }
             if (url==="div/Movimientos.html"){
-                document.querySelector('.registrarse').addEventListener("click", function(){mostrarpag("server/registrar.html")});
             }
           })
   
